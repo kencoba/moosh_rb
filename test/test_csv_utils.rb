1e954a6a65754f8088bc334c13c670f4f16639aa
Kobayachi Kenichi <kobayashi@primo.mamezou.com>
2020-08-27 18:11:36 +0900

    add test for CSVUtils::restriction

--- test/test_csv_utils.rb
+++ test/test_csv_utils.rb
@@ -32,4 +32,15 @@ class CSVUtilsTest < Test::Unit::TestCase
 	assert_equal(3,actual.size,"the result table should contain the rows which hc = 3(2 rows) and hc = 6 (1 row).")
 	
   end
+  
+  def test_restriction
+    row_a_1 = CSV::Row.new(["ha","hb","hc"],["1","2","3"])
+    row_a_2 = CSV::Row.new(["ha","hb","hc"],["4","5","6"])
+    row_a_3 = CSV::Row.new(["ha","hb","hc"],["7","8","9"])
+    table_a = CSV::Table.new([row_a_1,row_a_2,row_a_3])
+  
+	actual = CSVUtils::restriction(table_a) {|row| row["hb"].to_i > 4}
+  
+    assert_equal(2,actual.size,"the result rows of the table should contain hb = 5 or hb = 8")
+  end
 end
